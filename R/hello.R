0c0f84abfd1a3ab29ac9bbe0cf32fe4764c629b7
Carlos Alberto Silva <carlos_engflorestal@outlook.com>
2020-01-27 18:35:51 -0300

    test

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
-# Hello, world!
+# Hello, world!!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
