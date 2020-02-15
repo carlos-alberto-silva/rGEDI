66af141e4069b34b22eddb37d978dcc1b51215e9
caiohamamura <caiohamamura@gmail.com>
2020-02-15 02:15:21 -0300

    Change gediMetrics to use main() like function from gediMetrics: It now parses R input to argv and argc and uses the main function

---
+++ src/argParse.h
@@ -0,0 +1,14 @@
+#ifndef RGEDI_ARGPARSE_H
+#define RGEDI_ARGPARSE_H
+#include <Rinternals.h>
+
+#define PARSE_ARG(thename, n) thename##Arg(argv, &argc, #n, n)
+
+void charArg(char **argv, int *pos, const char *argName, SEXP in);
+void integerArg(char **argv, int *pos, const char *argName, SEXP in);
+void integerArrayArg(char **argv, int *pos, const char *argName, SEXP in);
+void logicalArg(char **argv, int *pos, const char *argName, SEXP in);
+void putArgName(char **argv, int *pos, const char *argName);
+void realArg(char **argv, int *pos, const char *argName, SEXP in);
+void realArrayArg(char **argv, int *pos, const char *argName, SEXP in);
+#endif /* RGEDI_ARGPARSE_H */
