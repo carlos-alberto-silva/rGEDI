4b34c8f32952e24ae609eca14d64268e95697316
caiohamamura <caiohamamura@gmail.com>
2020-02-21 15:18:03 -0300

    Rename R wrappers to C functions, prefix C:  - Parse aguments in simulation  - Document functions

--- R/argParse.R
+++ R/argParse.R
@@ -1,3 +1,13 @@
+stopifnotMessage = function(msg, ...) {
+  tryCatch(
+    stopifnot(...),
+    error=function(e) {
+      stop(paste0("\n\nInput arguments are invalid!\n",
+                  e$message))
+    }
+  )
+}
+
 checkNumericLength = function(x, len) {
   return (is.null(x) || (length(x) == len && is.numeric(x)))
 }
@@ -13,4 +23,26 @@ checkLogical = function(x) {
 checkInteger = function(x) {
   x_int = as.integer(x)
   return (is.null(x) || (length(x_int) == 1 && is.integer(x_int) && !is.na(x_int)))
+}
+
+checkCharacter = function(x) {
+  return (is.null(x) || (length(x)) == 1 && is.character(x))
+}
+
+checkFilepath = function(x, newFile=TRUE, optional = TRUE) {
+  exists = TRUE
+  if (is.null(x)) {
+    if (optional)
+      return (TRUE)
+    else
+      return (FALSE)
+  }
+  if (!is.character(x) || length(x) != 1)
+    return (FALSE)
+
+
+  if (!newFile)
+    return (file.exists(x))
+
+  return TRUE
 }
