7cb0caac0bd8c530a0564a03370b8d630ee0ff97
caiohamamura <caiohamamura@gmail.com>
2020-03-06 17:39:30 -0300

    Add general documentation for package

---
+++ R/rGEDI-package.R
@@ -0,0 +1,12 @@
+#'rGEDI: An R Package for NASA's Global Ecosystem Dynamics Investigation (GEDI) data downloading, visualizing and processing.
+#'
+#'@author Carlos A. Silva, Caio Hamamura, Ruben Valbuena, Steve Hancock, Adrian Cardil, Eben Broadbent, Danilo R. A. de Almeida, Celso H. L. Silva Junior and Carine Klauberg
+#'
+#'The rGEDI package provides functions for i) downloading, ii) visualizing, iii) clipping, iv) gridding, iv) simulating and v) exporting GEDI data.
+#'
+#'For comprehensive examples refer to \href{../doc/tutorial.html}{\code{vignette("Tutorial", package = "rGEDI")}}
+
+#'
+#'@docType package
+#'@name rGEDI-package
+NULL
