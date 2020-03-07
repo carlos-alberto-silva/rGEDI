8001bf9ba6e276057f2b6c4abe243e57334e2e6d
caiohamamura <caiohamamura@gmail.com>
2020-03-07 11:00:48 -0300

    Fix importing sp

--- R/clipTools.R
+++ R/clipTools.R
@@ -1,3 +1,6 @@
+#'@import sp
+NULL
+
 createAttributesWithinGroup = function(h5, newFile, group="/") {
   for (attr in hdf5r::list.attributes(h5[[group]])) {
     hdf5r::h5attr(newFile[[group]], attr) = hdf5r::h5attr(h5[[group]], attr)
