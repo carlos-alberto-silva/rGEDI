9c56b8d866005b6b26075f2e3c4582472e085603
caiohamamura <caiohamamura@gmail.com>
2020-03-07 15:48:24 -0300

    Close hdf5 files before unloading rGEDI

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,12 @@
 .onUnload <- function (libpath) {
+  objs = c("gedi.level1a", "gedi.level1b", "gedi.level2a", "gedi.level1bSim")
+  classes = sapply(ls(.GlobalEnv), function(x) class(get(x)))
+  classes = classes[classes %in% objs]
+  objNames = names(classes)
+  for (o in objNames) {
+    try(close(get(o)), silent=T)
+  }
+  rm(list=as.vector(objNames))
   library.dynam.unload("rGEDI", libpath)
   invisible()
 }
