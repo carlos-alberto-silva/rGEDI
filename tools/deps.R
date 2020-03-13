4c46dbacbc680ddeda2788141cd54e6a9ec89491
caiohamamura <caiohamamura@gmail.com>
2020-03-13 15:37:09 -0300

    Update gedisimulator code deps

--- tools/deps.R
+++ tools/deps.R
@@ -16,7 +16,7 @@ downloadDepBitBucket = function(name, file, origName) {
   fileCheck = file.path(".",name,file)
   if(!file.exists(fileCheck)) {
     print(paste0("Downloading ",name,"..."))
-    url = paste0("https://bitbucket.org/caiohamamura/",name,"/get/v0.3.0.zip")
+    url = paste0("https://bitbucket.org/caiohamamura/",name,"/get/v0.3.1.zip")
     download.file(url, "lib.zip", quiet=FALSE)
     unzip("lib.zip", exdir=".")
     unlink("lib.zip")
@@ -30,12 +30,12 @@ downloadDep("cmpfit-1.2",
             "https://www.physics.wisc.edu/~craigm/idl/down/cmpfit-1.2.tar.gz")
 downloadDep("libclidar",
 	    "libLasProcess.h",
-	    "https://github.com/caiohamamura/libclidar/archive/v0.1.tar.gz",
-	    "libclidar-0.1")
+	    "https://github.com/caiohamamura/libclidar/archive/v0.1.1.tar.gz",
+	    "libclidar-0.1.1")
 downloadDepBitBucket("gedisimulator",
                      "gediRat.c",
-                     "caiohamamura-gedisimulator-d0a94f85c41c")
+                     "caiohamamura-gedisimulator-24e4e4e84eb4")
 downloadDep("tools",
             "tools.c",
-            "https://github.com/caiohamamura/tools/archive/v0.1.tar.gz",
-	    "tools-0.1")
+            "https://github.com/caiohamamura/tools/archive/v0.1.1.tar.gz",
+	    "tools-0.1.1")
