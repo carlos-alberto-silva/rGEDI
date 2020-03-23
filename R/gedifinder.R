73c1bee359fd3045d9f5602654032b82da6b1349
caiohamamura <caiohamamura@gmail.com>
2020-03-23 13:42:47 -0300

    gedifinder: changed level parameter to product

--- R/gedifinder.R
+++ R/gedifinder.R
@@ -2,7 +2,7 @@
 #'
 #'@description This function finds the path to GEDI data within a boundary box coordinates provided
 #'
-#'@param level GEDI data level; Options: "GEDI01_B", "GEDI02_A" or "GEDI02_B"
+#'@param product GEDI data level; Options: "GEDI01_B", "GEDI02_A" or "GEDI02_B"
 #'@param ul_lat Numeric. Upper left (ul) corner coordinates, in lat (decimal degrees) for the bounding box of the area of interest.
 #'@param ul_lon Numeric. Upper left (ul) corner coordinates, in lon (decimal degrees) for the bounding box of the area of interest.
 #'@param lr_lat Numeric. Lower right (ul) corner coordinates, in lat (decimal degrees) for the bounding box of the area of interest.
@@ -27,15 +27,15 @@
 #'lr_lon<- -96.0
 #'
 #'# Extracting the path to GEDI data for the specified boundary box coordinates
-#'gedi02b_list<-gedifinder(level="GEDI02_B",ul_lat, ul_lon, lr_lat, lr_lon)
+#'gedi02b_list<-gedifinder(product="GEDI02_B",ul_lat, ul_lon, lr_lat, lr_lon)
 #'}
 #'@import jsonlite curl
 #'@export
-gedifinder<-function(level, ul_lat, ul_lon, lr_lat, lr_lon, version="001"){
+gedifinder<-function(product, ul_lat, ul_lon, lr_lat, lr_lon, version="001"){
   response = curl::curl(sprintf(
     "https://lpdaacsvc.cr.usgs.gov/services/gedifinder?%s=%s&%s=%s&%s=%f,%f,%f,%f&output=json",
     "version",version,
-    "product", level,
+    "product", product,
     "bbox", ul_lat, ul_lon, lr_lat,lr_lon))
   content = suppressWarnings(readLines(response))
   close(response)
