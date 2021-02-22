c77a82a7f2c30324c49422fde99bfffa907f6e59
Caio Hamamura <caiohamamura@gmail.com>
2021-02-22 17:21:23 -0300

    Change to containers

--- src/gdal.hpp
+++ src/gdal.hpp
@@ -3,6 +3,146 @@
 
 using namespace Rcpp;
 
+GDALDataset *create_dataset(const char *output, int nbands, int datatype, const char *projection, double lat_min, double lat_max, double lon_min, double lon_max, std::vector<double> res, double nodata, CharacterVector co);
+
+class GDALRasterBandR
+{
+private:
+  GDALRasterBand *band = NULL;
+
+public:
+  GDALRasterBandR(GDALRasterBand *the_band)
+  {
+    band = the_band;
+  }
+
+  int GetBlockXSize()
+  {
+    int result = 0;
+    band->GetBlockSize(&result, NULL);
+    return result;
+  }
+
+  int GetBlockYSize()
+  {
+    int result = 0;
+    band->GetBlockSize(NULL, &result);
+    return result;
+  }
+
+  template <typename S, typename T>
+  S ReadBlock(int iXBlock, int iYBlock)
+  {
+    CPLErr res = CE_None;
+    S output = NULL;
+
+    int nXBlockSize, nYBlockSize;
+    band->GetBlockSize(&nXBlockSize, &nYBlockSize);
+    if (std::is_same<T, GInt32>::value || std::is_same<T, double>::value)
+    {
+      S vec(nXBlockSize * nYBlockSize);
+      res = band->ReadBlock(iXBlock, iYBlock, vec.begin());
+      output = vec;
+    }
+    else
+    {
+      std::vector<T> buffer(nXBlockSize * nYBlockSize);
+      res = band->ReadBlock(iXBlock, iYBlock, buffer.data());
+      S out(buffer.begin(), buffer.end()));
+      output = out;
+    }
+
+    if (res == CE_Failure)
+      Rcpp::stop(CPLGetLastErrorMsg());
+
+    return output;
+  }
+
+  template <typename T>
+  void WriteBlock(int iXBlock, int iYBlock, T buffer)
+  {
+    GDALDataType dtype = band->GetRasterDataType();
+    CPLErr res = CE_None;
+
+    switch (dtype)
+    {
+    case GDALDataType::GDT_UInt16:
+    {
+      std::vector<GUInt16> vec(buffer.begin(), buffer.end());
+      res = band->WriteBlock(iXBlock, iYBlock, vec.data());
+      break;
+    }
+
+    case GDALDataType::GDT_Int16:
+    {
+      std::vector<GInt16> vec(buffer.begin(), buffer.end());
+      res = band->WriteBlock(iXBlock, iYBlock, vec.data());
+      break;
+    }
+
+    case GDALDataType::GDT_UInt32:
+    {
+      std::vector<GUInt32> vec(buffer.begin(), buffer.end());
+      res = band->WriteBlock(iXBlock, iYBlock, vec.data());
+      break;
+    }
+
+    case GDALDataType::GDT_Float32:
+    {
+      std::vector<float> vec(buffer.begin(), buffer.end());
+      res = band->WriteBlock(iXBlock, iYBlock, vec.data());
+      break;
+    }
+
+    default:
+    {
+      res = band->WriteBlock(iXBlock, iYBlock, buffer.begin());
+      break;
+    }
+    }
+
+    if (res == CE_Failure)
+      Rcpp::stop(CPLGetLastErrorMsg());
+
+    res = band->FlushBlock(iXBlock, iYBlock, 1);
+
+    if (res == CE_Failure)
+      Rcpp::stop(CPLGetLastErrorMsg());
+  }
+};
+
+class GDALDatasetR
+{
+private:
+  GDALDataset *ds = NULL;
+
+public:
+  GDALDatasetR(const char *output, int nbands, int datatype, const char *projection, double lat_min, double lat_max, double lon_min, double lon_max, std::vector<double> res, double nodata, CharacterVector co)
+  {
+    ds = create_dataset(output, nbands, datatype, projection, lat_min, lat_max, lon_min, lon_max, res, nodata, co);
+  };
+
+  GDALRasterBandR GetRasterBand(int nband)
+  {
+    return GDALRasterBandR(ds->GetRasterBand(nband));
+  }
+
+  int GetRasterXSize()
+  {
+    return ds->GetRasterXSize();
+  }
+
+  int GetRasterYSize()
+  {
+    return ds->GetRasterYSize();
+  }
+
+  void Close()
+  {
+    GDALClose(GDALDataset::ToHandle(ds));
+  }
+};
+
 GDALDataset *create_dataset(const char *output, int nbands, int datatype, const char *projection, double lat_min, double lat_max, double lon_min, double lon_max, std::vector<double> res, double nodata, CharacterVector co)
 {
   CPLErr err = CE_None;
@@ -150,30 +290,29 @@ void RGDALClose(GDALDataset *ds)
 RCPP_MODULE(gdal_module)
 {
 
-  class_<GDALDataset>("CPP_GDALDataset")
-      .method("GetRasterBand", &GDALDataset::GetRasterBand)
-      .method("GetRasterXSize", &GDALDataset::GetRasterXSize)
-      .method("GetRasterYSize", &GDALDataset::GetRasterYSize)
-      .method("Close", &RGDALClose);
+  class_<GDALDatasetR>("CPP_GDALDataset")
+      .method("GetRasterBand", &GDALDatasetR::GetRasterBand)
+      .method("GetRasterXSize", &GDALDatasetR::GetRasterXSize)
+      .method("GetRasterYSize", &GDALDatasetR::GetRasterYSize)
+      .method("Close", &GDALDatasetR::Close);
 
-  class_<GDALRasterBand>("CPP_GDALRasterBand")
-      .method("GetXSize", &GDALRasterBand::GetXSize)
-      .method("ReadBlock1", &ReadBlock<GByte, RawVector>)
-      .method("ReadBlock2", &ReadBlock<GUInt16, IntegerVector>)
-      .method("ReadBlock3", &ReadBlock<GInt16, IntegerVector>)
-      .method("ReadBlock4", &ReadBlock<GUInt32, IntegerVector>)
-      .method("ReadBlock5", &ReadBlock<GInt32, IntegerVector>)
-      .method("ReadBlock6", &ReadBlock<float, NumericVector>)
-      .method("ReadBlock7", &ReadBlock<double, NumericVector>)
-      .method("WriteBlock1", &WriteBlock<RawVector>)
-      .method("WriteBlock2", &WriteBlock<IntegerVector>)
-      .method("WriteBlock3", &WriteBlock<IntegerVector>)
-      .method("WriteBlock4", &WriteBlock<IntegerVector>)
-      .method("WriteBlock5", &WriteBlock<IntegerVector>)
-      .method("WriteBlock6", &WriteBlock<NumericVector>)
-      .method("WriteBlock7", &WriteBlock<NumericVector>)
-      .method("GetBlockXSize", &GetBlockXSize)
-      .method("GetBlockYSize", &GetBlockYSize);
+  class_<GDALRasterBandR>("CPP_GDALRasterBand")
+      .method("ReadBlock1", &GDALRasterBandR::ReadBlock<GByte, RawVector>)
+      .method("ReadBlock2", &GDALRasterBandR::ReadBlock<GUInt16, IntegerVector>)
+      .method("ReadBlock3", &GDALRasterBandR::ReadBlock<GInt16, IntegerVector>)
+      .method("ReadBlock4", &GDALRasterBandR::ReadBlock<GUInt32, IntegerVector>)
+      .method("ReadBlock5", &GDALRasterBandR::ReadBlock<GInt32, IntegerVector>)
+      .method("ReadBlock6", &GDALRasterBandR::ReadBlock<float, NumericVector>)
+      .method("ReadBlock7", &GDALRasterBandR::ReadBlock<double, NumericVector>)
+      .method("WriteBlock1", &GDALRasterBandR::WriteBlock<RawVector>)
+      .method("WriteBlock2", &GDALRasterBandR::WriteBlock<IntegerVector>)
+      .method("WriteBlock3", &GDALRasterBandR::WriteBlock<IntegerVector>)
+      .method("WriteBlock4", &GDALRasterBandR::WriteBlock<IntegerVector>)
+      .method("WriteBlock5", &GDALRasterBandR::WriteBlock<IntegerVector>)
+      .method("WriteBlock6", &GDALRasterBandR::WriteBlock<NumericVector>)
+      .method("WriteBlock7", &GDALRasterBandR::WriteBlock<NumericVector>)
+      .method("GetBlockXSize", &GDALRasterBandR::GetBlockXSize)
+      .method("GetBlockYSize", &GDALRasterBandR::GetBlockYSize);
 
   function("create_dataset", &create_dataset);
 }
