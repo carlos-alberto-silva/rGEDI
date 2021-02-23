37e104d53b4ce4494d16b09d2cb697d902032c4e
Caio Hamamura <caiohamamura@gmail.com>
2021-02-22 22:00:16 -0300

    Return rasterband pointer

--- src/rasterizeHDF.cpp
+++ src/rasterizeHDF.cpp
@@ -5,8 +5,6 @@
 using namespace Rcpp;
 
 
-
-
 // You can include R code blocks in C++ files processed with sourceCpp
 // (useful for testing and development). The R code will be automatically
 // run after the compilation.
