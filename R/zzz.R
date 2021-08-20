ea4ed1bc318131af25459558b628493395f43748
Caio Hamamura <caiohamamura@gmail.com>
2021-08-20 11:02:22 -0300

    Update zzz to autoload InitializeGDAL

--- R/zzz.R
+++ R/zzz.R
@@ -3,16 +3,15 @@
   invisible()
 }
 
+#' @export CPP_GDALDataset CPP_GDALRasterBand create_dataset
 .onLoad <- function(libname, pkgname) {
   .RGEDI_CACHE <- new.env(FALSE, parent=globalenv())
   assign("old.PROJ_LIB", Sys.getenv("PROJ_LIB"), envir=.RGEDI_CACHE)
+  Rcpp::loadModule("gdal_module", TRUE, TRUE)
+  InitializeGDAL()
 }
 
 .onAttach <- function(libname, pkgname) {
   #Setup copied from rgdal package
   Sys.setenv("PROJ_LIB"=system.file("proj", package = pkgname)[1])
-    InitializeGDAL()
 }
-
-#' @export CPP_GDALDataset CPP_GDALRasterBand create_dataset
-Rcpp::loadModule("gdal_module", TRUE)
